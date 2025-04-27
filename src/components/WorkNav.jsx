138219d4950c4264e9921afb0554468cc0406363
codinBabe <teboola2@gmail.com>
2025-04-27 13:54:41 +0100

    feat:completed about desktop view

--- src/components/WorkNav.jsx
+++ src/components/WorkNav.jsx
@@ -1,9 +1,15 @@
 import { NavLink } from "react-router-dom";
 import { TickIcon } from "../utils/icons/TickIcon";
+import { motion } from "framer-motion";
 
 const WorkNav = () => {
   return (
-    <nav className="text-sm text-greyblack300 flex items-center justify-center -ml-8 fixed top-20 mt-4 z-20 w-full">
+    <motion.nav
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1.2, ease: "easeInOut" }}
+      className="text-sm text-greyblack300 flex items-center justify-center -ml-8 fixed top-20 mt-4 z-20 w-full"
+    >
       <NavLink
         className={({ isActive }) =>
           `border border-greyblack400 p-2 rounded-tl-full rounded-bl-full transition-all flex items-center gap-2 whitespace-nowrap ${
@@ -83,7 +89,7 @@ const WorkNav = () => {
           </>
         )}
       </NavLink>
-    </nav>
+    </motion.nav>
   );
 };
 
